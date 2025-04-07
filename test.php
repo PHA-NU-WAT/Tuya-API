eac655a9a236b84217bd770357974987216f307a
PHA-NU-WAT <121420476+PHA-NU-WAT@users.noreply.github.com>
2025-04-07 20:42:17 +0700

    Update test.php

--- test.php
+++ test.php
@@ -3,16 +3,16 @@ require 'TuyaCloud.php';
 
 $options = [
     'baseUrl' => 'https://openapi.tuyaus.com', // URL API of Tuya
-    'accessKey' => '73vh9uqn88dtxpv5k5fd', // access key of your app
-    'secretKey' => '9b8e31453ccc4d1c9a748be0565cfb2c', // access secret of your app
+    'accessKey' => 'xxxxxxxxxxxxxxxxxxxxxxx', // access key of your app
+    'secretKey' => 'xxxxxxxxxxxxxxxxxxxxxxx', // access secret of your app
 ];
 
 $tuya = new TuyaCloud($options);
 try {
     // to get the device status
     // you must pass the device_id
-    $device1 = $tuya->getDevice('eb969c65f92be3effeztqf');
-    $device2 = $tuya->getDevice('eb9f56ee79d69db886yszk');
+    $device1 = $tuya->getDevice('xxxxxxxxxxxxxxxxxxxxxxx');
+    $device2 = $tuya->getDevice('xxxxxxxxxxxxxxxxxxxxxxx');
     echo json_encode($device2);
 
 
